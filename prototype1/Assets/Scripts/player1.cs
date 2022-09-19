5f6c60ed76656e4180f30ccb1e8c99d978782883
whangashley <whangashley@gmail.com>
2022-09-19 11:22:53 -0400

    Hit Anims Done

--- prototype1/Assets/Scripts/player1.cs
+++ prototype1/Assets/Scripts/player1.cs
@@ -11,16 +11,22 @@ public class player1 : MonoBehaviour
     //Rigidbody2D player1Body;
 
     public float knockbackTimeP2;
-    public bool player2Hit;
+
+    player2 player2Script;
+    public GameObject objplayer2;
+
+    public bool player2Stun;
 
     public Animator animator1;
+    public Animator animator2;
     // Start is called before the first frame update
     void Start()
     {
         //setting player 1's rigidbody into the variable
         //player1Body = gameObject.GetComponent<Rigidbody2D>();
         //knockbackTimeP2 = 0.6f;
-        player2Hit = false;
+        player2Script = objplayer2.GetComponent<player2>();
+        player2Stun = false;
         animator1.SetBool("player1Moving", false);
     }
 
@@ -28,30 +34,33 @@ public class player1 : MonoBehaviour
     void Update()
     {
         animator1.SetBool("player1Moving", false);
-        //if player 1 presses W
-        if (Input.GetKey(KeyCode.W)) {
-            if (transform.position.y < 5f) {
-                //then player 1 will move up
-                Move(Vector3.up);
-            }
-        }
-        //else if player 1 presses S
-        else if (Input.GetKey(KeyCode.S)) {
-            //and if the player's y position is above the bottom of the screen
-            if (transform.position.y > -4.6f) {
-                //then player 1 will move down
-                Move(Vector3.down);
-            }
-        }
 
-        if (Input.GetKey(KeyCode.D)) {
-            if (transform.position.x < 6.6f) {
-                Move(Vector3.right);
+        if (player2Script.player1Stun == false) {
+            //if player 1 presses W
+            if (Input.GetKey(KeyCode.W)) {
+                if (transform.position.y < 5f) {
+                    //then player 1 will move up
+                    Move(Vector3.up);
+                }
             }
-        }
-        else if (Input.GetKey(KeyCode.A)) {
-            if (transform.position.x > -6f) {
-                Move(Vector3.left);
+            //else if player 1 presses S
+            else if (Input.GetKey(KeyCode.S)) {
+                //and if the player's y position is above the bottom of the screen
+                if (transform.position.y > -4.6f) {
+                    //then player 1 will move down
+                    Move(Vector3.down);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.D)) {
+                if (transform.position.x < 6.6f) {
+                    Move(Vector3.right);
+                }
+            }
+            else if (Input.GetKey(KeyCode.A)) {
+                if (transform.position.x > -6f) {
+                    Move(Vector3.left);
+                }
             }
         }
     }
@@ -65,8 +74,10 @@ public class player1 : MonoBehaviour
     //if the player collides with an enemy
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "player2") {
+            animator2.SetBool("player2Hit", true);
             Rigidbody2D player2Body = collision.GetComponent<Rigidbody2D>();
             Debug.Log("i hit player 2");
+            player2Stun = true;
             //set kinematic to dynamic so force is applied (gravity on)
             player2Body.isKinematic = false;
             Vector2 difference = player2Body.transform.position - transform.position;
@@ -79,5 +90,7 @@ public class player1 : MonoBehaviour
         yield return new WaitForSeconds(knockbackTimeP2);
         player2.velocity = Vector2.zero;
         player2.isKinematic = true;
+        animator2.SetBool("player2Hit", false);
+        player2Stun = false;
     }
 }
--- prototype1/Assets/Scripts/player2.cs
+++ prototype1/Assets/Scripts/player2.cs
@@ -17,16 +17,22 @@ public class player2 : MonoBehaviour
     //Rigidbody2D player2Body;
 
     public float knockbackTimeP1;
-    public bool player1Hit;
+
+    player1 player1Script;
+    public GameObject objplayer1;
+
+    public bool player1Stun;
 
     public Animator animator2;
+    public Animator animator1;
     // Start is called before the first frame update
     void Start()
     {
         //setting player 2's rigidbody into the variable
         //player2Body = gameObject.GetComponent<Rigidbody2D>();
         //knockbackTimeP1 = 0.6f;
-        player1Hit = false;
+        player1Script = objplayer1.GetComponent<player1>();
+        player1Stun = false;
         animator2.SetBool("player2Moving", false);
     }
 
@@ -34,30 +40,33 @@ public class player2 : MonoBehaviour
     void Update()
     {
         animator2.SetBool("player2Moving", false);
-        //if player 2 presses up arrow key
-        if (Input.GetKey(KeyCode.UpArrow)) {
-            if (transform.position.y < 5f) {
-                //then player 2 will move up
-                Move(Vector3.up);
-            }
-        }
-        //else if player 2 presses down arrow key
-        else if (Input.GetKey(KeyCode.DownArrow)) {
-            //and if the player's y position is above the bottom of the screen
-            if (transform.position.y > -4.6f) {
-                //then player 2 will move down
-                Move(Vector3.down);
-            }
-        }
 
-        if (Input.GetKey(KeyCode.RightArrow)) {
-            if (transform.position.x < 6.6f) {
-                Move(Vector3.right);
+        if (player1Script.player2Stun == false) {
+            //if player 2 presses up arrow key
+            if (Input.GetKey(KeyCode.UpArrow)) {
+                if (transform.position.y < 5f) {
+                    //then player 2 will move up
+                    Move(Vector3.up);
+                }
             }
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow)) {
-            if (transform.position.x > -6f) {
-                Move(Vector3.left);
+            //else if player 2 presses down arrow key
+            else if (Input.GetKey(KeyCode.DownArrow)) {
+                //and if the player's y position is above the bottom of the screen
+                if (transform.position.y > -4.6f) {
+                    //then player 2 will move down
+                    Move(Vector3.down);
+                }
+            }
+
+            if (Input.GetKey(KeyCode.RightArrow)) {
+                if (transform.position.x < 6.6f) {
+                    Move(Vector3.right);
+                }
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow)) {
+                if (transform.position.x > -6f) {
+                    Move(Vector3.left);
+                }
             }
         }
     }
@@ -71,8 +80,10 @@ public class player2 : MonoBehaviour
     //if the player hits an enemy
     void OnTriggerEnter2D(Collider2D collision) {
         if (collision.gameObject.tag == "player1") {
+            animator1.SetBool("player1Hit", true);
             Rigidbody2D player1Body = collision.GetComponent<Rigidbody2D>();
             Debug.Log("i hit player 1");
+            player1Stun = true;
             //set kinematic to dynamic so force is applied (gravity on)
             player1Body.isKinematic = false;
             Vector2 difference = player1Body.transform.position - transform.position;
@@ -85,5 +96,7 @@ public class player2 : MonoBehaviour
         yield return new WaitForSeconds(knockbackTimeP1);
         player1.velocity = Vector2.zero;
         player1.isKinematic = true;
+        animator1.SetBool("player1Hit", false);
+        player1Stun = false;
     }
 }
